997517bc712232aa069a2cd36c4310b77ef1b428
CopPret <123130760+CopPret@users.noreply.github.com>
2023-02-02 13:02:37 -0500

    Configuring Apple Script
    Keep trying to add script to apple but get error message

--- Assets/01-Apple Picker/AppleTree.cs
+++ Assets/01-Apple Picker/AppleTree.cs
@@ -68,3 +68,28 @@ public class AppleTree : MonoBehaviour
     }
 
 }
+
+public class AppleTree : MonoBehaviour {
+
+    void Start () {
+
+        // Dropping apples every second
+
+        Invoke( "DropApple", 2f );
+
+    }
+
+    void DropApple() {
+
+        GameObject apple = Instantiate<GameObject;
+        
+        apple.transform.position = transform.position;
+
+        Invoke( "DropApple", secondsBetweenAppleDrop);
+
+    }    
+
+void Update () {}
+
+    }
+}
